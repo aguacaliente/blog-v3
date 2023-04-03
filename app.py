d56cd8f4f78bdaa994923445ee09abfa7f98a2f5
aguacaliente <98557628+aguacaliente@users.noreply.github.com>
2023-04-03 18:06:58 +0100

    jobitem function added

--- app.py
+++ app.py
@@ -3,9 +3,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+POSTS = [
+    {
+    'id' : 1,
+    'title': 'Quicker CSS with Bootstrap ',
+    'author': 'John Doe',
+    'tagline': ''
+},
+    {
+    'id' : 2,
+    'title': 'API endpoints ',
+    'author': 'Jane Doe',
+    'tagline': ''
+},
+    {
+    'id' : 3,
+    'title': 'React and CSS ',
+    'author': 'Jane Doe',
+    'tagline': ''
+},
+    {
+    'id' : 4,
+    'title': 'React and CSS ',
+    'author': 'Betty Swallocks',
+    'tagline': 'author of getting laid a nerds guide'
+},
+    ]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html',
+                           posts=POSTS)
+
 
 
 if __name__ == "__main__":
